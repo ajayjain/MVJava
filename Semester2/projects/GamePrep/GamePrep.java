f0efc9c2a15043a624bca6279c461db3310c74f5
Ajay Jain <ajayjain318@gmail.com>
2013-04-07 21:20:52 -0700

    GamePrep

--- Semester2/projects/GamePrep/GamePrep.java
+++ Semester2/projects/GamePrep/GamePrep.java
@@ -7,12 +7,16 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
+
+import java.io.IOException;
 
 public class GamePrep {
 	private ControlPanel controls;
 	private ButtonPanel buttons;
-	private LeadTablePanel leadTable;
 	private JFrame frame;
+	private GameRouter router;
+	private JSlider cardSlider;
 	
 	public static void main (String[] args) {
 		GamePrep gpa = new GamePrep();
@@ -23,7 +27,7 @@ public class GamePrep {
 		// Initialize and set up the JFrame
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(480, 640);
+		frame.setSize(640, 640);
 
 		// Create the colors panel and buttons panel
 		JPanel colorPicker = new JPanel();
@@ -36,19 +40,76 @@ public class GamePrep {
 
 		colorPicker.add(colorPickerHeader, BorderLayout.NORTH);
 
-		ColorPanel colors = new ColorPanel();
 		buttons = new ButtonPanel();
 		controls = new ControlPanel();
-		leadTable = new LeadTablePanel();
-		
-		
-		// add the JPanels to the frame
+
 		frame.getContentPane().add(controls, BorderLayout.NORTH);
-		frame.getContentPane().add(leadTable, BorderLayout.CENTER);
+		router = new GameRouter();
+		frame.getContentPane().add(router, BorderLayout.CENTER);
 		frame.getContentPane().add(buttons, BorderLayout.SOUTH);
 
 		// make the frame visible
 		frame.setVisible(true);
+	}
+
+	class GameRouter extends JPanel {
+		private LeadTablePanel leadTable;
+		private ColorPanel colors;
+		private Pacman pac;
+
+		public GameRouter() {
+			super();
+			setLayout(new BorderLayout());
+
+			// Leaderboard is the default page
+			leadTable = new LeadTablePanel();
+			showLeaderboard();
+		}
+
+		public void launchGame(String game) {
+			// Clean up
+			removeAll();
+			updateUI();
+			colors = null;
+			pac = null;
+
+			if (game.equals("Color Picker")) {
+				JLabel top = new JLabel(" Choose a color:");
+				top.setFont(new Font("Serif", Font.PLAIN, 20));
+				add(top, BorderLayout.NORTH);
+
+				colors = new ColorPanel();
+				add(colors);
+
+				cardSlider = new JSlider(1, 3, 0);
+				cardSlider.setOrientation(JSlider.VERTICAL);
+				cardSlider.addChangeListener(colors);
+				add(cardSlider, BorderLayout.EAST);
+			} else if (game.equals("Pacman (panel)")) {
+				pac = new Pacman();
+				pac.run();
+				frame.addKeyListener(pac.panel);
+				add(pac.panel);
+			} else {
+				showLeaderboard();
+				launchProject(game);
+			}
+		}
+
+		private void launchProject(String proj) {
+			try {
+				Runtime.getRuntime().exec(new String[] {"java", "-cp", "../"+proj, proj});
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+				System.exit(1);
+			}
+		}	
+
+		public void showLeaderboard() {
+			removeAll();
+			updateUI();
+			add(leadTable);
+		}
 	}
 	
 	class ControlPanel extends JPanel implements ActionListener {
@@ -68,26 +129,41 @@ public class GamePrep {
 		}
 		
 		public void actionPerformed(ActionEvent e) {
-			frame.getContentPane().remove(colors);
-			frame.getContentPane().add(leadTable, BorderLayout.CENTER);
-			colors.setVisible(false);
-			leadTable.setVisible(true);
-			if (e.getActionCommand().equals("NEW GAME")) {
-				// if (!launched) {
-					frame.getContentPane().remove(leadTable);
-					frame.getContentPane().add(colors, BorderLayout.CENTER);
-					leadTable.setVisible(false);
-					colors.setVisible(true);
-				// }
-			}
-			if (e.getActionCommand().equals("LEADERBOARD")) {
-				// if (launched) {
-
-				// }
-			}
+			if (e.getActionCommand().equals("NEW GAME"))
+				// Switch to first game - color picker
+				router.launchGame("Color Picker");
+			else if (e.getActionCommand().equals("LEADERBOARD"))
+				router.showLeaderboard();
 		}
 	}
+
+	class ButtonPanel extends JPanel implements ActionListener {
+		private JButton[] gameButtons;
+		private String[] gameNames =
+			{"Color Picker", "Pacman (panel)", "Pacman", "Trail", "Draw", "Web", "Quote", "Calvin"};
+		
+		public ButtonPanel() {
+			// Change FlowLayout (default) to GridLayout
+			this.setLayout(new GridLayout(2, gameNames.length/2));
+			// Initialize buttons
+			gameButtons = new JButton[gameNames.length];
+			for (int b = 0; b < gameNames.length; b++) {
+				gameButtons[b] = new JButton (gameNames[b]);
+				gameButtons[b].addActionListener(this);
+				this.add(gameButtons[b]);
+			}
+		}
+		
+		public void actionPerformed (ActionEvent a) {
+			router.launchGame(a.getActionCommand());
+		}
+	}
+
+	///////////
+	// GAMES //
+	///////////
 	
+	// Panel containing table of scores
 	class LeadTablePanel extends JPanel {
 		private String[] columnNames = {"Username", "Date", "Score", "Color"};
 		private Object[][] data = {
@@ -97,77 +173,21 @@ public class GamePrep {
 		};
 		
 		public LeadTablePanel() {
-			// setLayout(new GridLayout(10,3));
-			// add(new JLabel("Ajay"));
 			JTable table = new JTable(data, columnNames);
 			add(table);
 		}
 	}
-	
-	class FillPanel extends JPanel    {
-       JPanel panel1, panel2, panel3;
-       Drawer drawer1;
-       JTextArea area;
-       JTextField field;
-       JScrollBar bar;
-       JButton button1, button2;
-       
-       public FillPanel ( ) {
-	       this.setLayout(new GridLayout(2,1));
-	       panel1 = new JPanel();
-	       panel1.setLayout(new GridLayout(2,1));
-	       
-	       panel2 = new JPanel();
-	       area = new JTextArea(8,8);
-	       field = new JTextField("good");
-	       bar = new JScrollBar( JScrollBar.HORIZONTAL, 1, 1, 1, 20 );
-	       
-	       panel2.add(area);
-	       panel2.add(field);
-	       panel2.add(bar);
-	       
-	       panel3 = new JPanel();
-	       panel3.setLayout(new BorderLayout());
-	       
-	       button1 = new JButton("Java");
-	       button2 = new JButton("Programmer");
-	       
-	       panel3.add(button1,BorderLayout.EAST);
-	       panel3.add(button2,BorderLayout.CENTER);
-	       
-	       panel1.add(panel2);
-	       panel1.add(panel3);
-	       
-	       this.add(panel1);
-	       drawer1 = new Drawer();
-	       this.add(drawer1);
-       }
-	}
-	
-	class Drawer extends JPanel{
-       public Drawer ( )   {
-	       setBackground(Color.RED);
-       }
-       public void paintComponent ( Graphics g ) {
-	       super.paintComponent(g);
-       }
-	}
 
-
-	
-	class ColorPanel extends JPanel implements MouseListener {
-		//////////////////////////////////////////////////////////
+	class ColorPanel extends JPanel implements MouseListener, ChangeListener {
 		// Declare CardLayout and JPanel objects
 		private CardLayout cards;
 		private JPanel color1, color2, color3;
 		
-		public ColorPanel() {			// constructor
-			////////////////////////////////////////////////////////
+		public ColorPanel() {
 			// Initialize CardLayout and setLayout() to this panel
 			cards = new CardLayout();
 			this.setLayout(cards);
 			
-			////////////////////////////////////////////////////////
 			// Create blue, green and red JPanels and set their backgrounds
 			color1 = new JPanel();
 			color1.setBackground(Color.blue);
@@ -178,9 +198,7 @@ public class GamePrep {
 			color3 = new JPanel();
 			color3.setBackground(Color.red);
 
-			////////////////////////////////////////////////////////
-			// Add the MouseListener to *each* JPanel and add
-			// each JPanel to ColorPanel (this)
+			// Add the MouseListener to *each* JPanel and add each JPanel to ColorPanel (this)
 			color1.addMouseListener(this);
 			color2.addMouseListener(this);
 			color3.addMouseListener(this);
@@ -193,41 +211,14 @@ public class GamePrep {
 			////////////////////////////////////////////////////////
 			// Select the next panel on the stack of panels
 			cards.next(this);
+			cardSlider.setValue((cardSlider.getValue() + 1) % 3);
+		}
+		public void stateChanged(ChangeEvent e) {
+			cards.show(this, "Panel "+cardSlider.getValue());
 		}
 		public void mouseEntered(MouseEvent evt) { }
 		public void mouseExited(MouseEvent evt) { }
 		public void mouseClicked(MouseEvent evt) { }
 		public void mouseReleased(MouseEvent evt) { }
-	}
-	
-	class ButtonPanel extends JPanel implements ActionListener {
-		private JButton lang1, lang2, lang3;
-		private JTextField langField;
-		private String[] languages = { "English", "French", "German" };
-		
-		public ButtonPanel() {
-			////////////////////////////////////////////////////////
-			// Change FlowLayout (default) to GridLayout
-			// with 1 row and 4 columns.
-			this.setLayout(new GridLayout(1,4));
-					
-			lang1 = new JButton (languages[0]);
-			lang1.addActionListener(this);
-			this.add(lang1);
-			lang2 = new JButton (languages[1]);
-			lang2.addActionListener(this); 		
-			this.add(lang2);
-			lang3 = new JButton (languages[2]);
-			lang3.addActionListener(this); 		
-			this.add(lang3);
-			langField = new JTextField(languages[0], 9);
-			this.add(langField);
-		}
-		
-		public void actionPerformed (ActionEvent a) {
-			String command = a.getActionCommand();
-			for (int i=0; i < 3; i++)
-				if (command.equals(languages[i])) langField.setText(languages[i]);
-		}
 	}
 }
---
+++ Semester2/projects/GamePrep/Pacman.java
@@ -0,0 +1,250 @@
+// Ajay Jain
+// March 4, 2013
+// Pacman.java
+// This is a simplified version of Pacman, where you try to eat all the cheese and avoid the monsters.
+
+import java.util.Random;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Pacman {
+	private byte[][][] board;	// Holds counts of cheese and monsters at each spot on a 10 by 10 board
+	private byte[] pacman;			// Data about pacman: [row, col, mouthAngle]
+	private boolean isOpeningMouth, isDead, isWinner;	// booleans tracking state of pacman
+	private Timer monsterTimer, pacmanTimer;	// Hold the timers for the game
+	private byte cheese;	// Counts how much cheese is on the board
+	private Font endFont;
+
+	public GamePanel panel;	// JPanel containing all the content
+	
+	public static void main(String[] args) {
+		Pacman game = new Pacman();
+		game.run();
+	}
+	
+	// Starting point for game
+	public void run() {
+		board = new byte[10][10][2];			//format of string: "cheesecount-monstercount" ("12" - 1 cheese, 2 monsters)
+		isOpeningMouth = isDead = isWinner = false;
+		cheese = 6;
+		pacman = new byte[3];
+		endFont = new Font("Helvetica", Font.PLAIN, 60);
+		
+		placeStuff(1);
+		placeStuff(2);
+		//System.out.println(java.util.Arrays.deepToString(board));
+		
+		panel = new GamePanel();
+		panel.setFocusable(true);
+		panel.requestFocusInWindow();
+	}
+		
+	// Place monsters, cheese and pacman randomly on the board
+	private void placeStuff(int statusIndex) {
+		Random rand = new Random();
+		for (int count = 0; count < 6; count++) {
+			int x, y;
+			switch (statusIndex) {
+				case 1:
+					// mark random cheese while not on unique square
+					do {
+						x = rand.nextInt(10);
+						y = rand.nextInt(10);
+					} while (board[x][y][0] > 0);
+					board[x][y][0] = 1;
+					break;
+				case 2:
+					// mark random monster
+					x = rand.nextInt(10);
+					y = rand.nextInt(10);
+					board[x][y][1]++;
+			}
+		}
+		// mark random pacman
+		byte x, y;
+		do {
+			x = (byte) rand.nextInt(10);
+			y = (byte) rand.nextInt(10);
+		} while (board[x][y][1] > 0); // while on top of a monster
+		pacman[0] = x;
+		pacman[1] = y;
+	}
+
+	// Main panel/canvas for game
+	class GamePanel extends JPanel implements KeyListener {
+		public GamePanel() {
+			super();
+			
+			// Initialize a timer to move monsters at 1 second intervals
+			monsterTimer = new Timer(1000, new MonsterLoop());
+			monsterTimer.start();
+			
+			// Start off with a 40 degree mouth angle
+			pacman[2] = 40;
+			pacmanTimer = new Timer(5, new PacmanLoop());
+			pacmanTimer.start();
+		}
+	
+		// Calls drawing methods
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			drawBackground(g);
+			drawGrid(g);
+		}
+		
+		// Draws the backgrounds and instructions
+		public void drawBackground(Graphics g) {
+			setBackground(Color.blue);
+			
+			// Fill rect for board outline
+			g.setColor(Color.lightGray);
+			g.fillRect(15, 15, 439, 439);
+			
+			// Bottom red rect for instructions
+			g.setColor(Color.red);
+			g.fillRect(0, 470, 500, 30);
+			g.setColor(Color.black);
+			g.drawString("Directions: a = left, d = right, w = up, s = down", 55, 485);
+		}
+		
+		// Draws white squares, cheese, pacman and monsters
+		public void drawGrid(Graphics g) {
+			for (int row = 0; row < 10; row++) {
+				for (int col = 0; col < 10; col++) {
+					byte[] status = board[row][col];
+					int y = 43*row+20,
+						x = 43*col+20;
+					//System.out.printf("%d %d %s\n", row, col, status);
+					// Draw white square
+					g.setColor(Color.white);
+					g.fillRect(x, y, 40, 40);
+					if (status[0] > 0) {
+						//if pacman is on this spot
+						if (pacman[0] == row && pacman[1] == col) {
+							board[row][col][0]--;
+							// if all cheese is eaten, we have a winner!
+							if (--cheese < 1)
+								isWinner = true;
+						} else {
+							//Draw cheese square
+							g.setColor(Color.yellow);
+							g.fillRect(x+2, y+2, 36, 36);
+						}
+					}
+					if (status[1] > 0) {
+						// Draw monster head
+						g.setColor(Color.red);
+						g.fillOval(x, y, 40, 40);
+						// Draw eyes
+						g.setColor(Color.black);
+						g.fillOval(x+10, y+13, 6, 6);
+						g.fillOval(x+24, y+13, 6, 6);
+						// Draw mouth
+						g.drawLine(x+10, y+30, x+30, y+30);
+						
+						if (pacman[0] == row && pacman[1] == col)
+							isDead = true; // Pacman dies!
+					}
+					// Draw pacman with either wide or narrow mouth depending on pacman[2]
+					y = 43*pacman[0]+20;
+					x = 43*pacman[1]+20;
+					g.setColor(Color.blue);
+					g.fillArc(x, y, 40, 40, pacman[2], 360-2*pacman[2]);
+				}
+			}
+			if (isWinner) {
+				stopTimers();
+				g.setColor(Color.blue);
+				g.setFont(endFont);
+				g.drawString("YOU WIN!" , 105, 250);
+			}
+			if (isDead) {
+				stopTimers();
+				g.setColor(Color.red);
+				g.setFont(endFont);
+				g.drawString("YOU LOSE!", 100, 250);	// Draw at end so a white square doesn't overlap it
+			}
+		}
+		
+		// Stop monster movement and pacman mouth timers
+		public void stopTimers() {
+			monsterTimer.stop();
+			pacmanTimer.stop();
+		}
+		
+		// update pacman location, params are change in location
+		public void movePacman(int drow, int dcol) {
+			if (!isWinner && !isDead) {
+				// go to the other side of the board if pacman passes the edge
+				int nrow = (pacman[0] += drow) % 10,
+					ncol = (pacman[1] += dcol) % 10;
+				if (nrow == -1) nrow = 9;
+				if (ncol == -1) ncol = 9;
+				
+				pacman[0] = (byte) nrow;
+				pacman[1] = (byte) ncol;
+			}
+		}
+		
+		// Decide what to do when a key is pressed
+		public void keyPressed(KeyEvent e) {
+			switch(e.getKeyChar()) {
+				case 'w': movePacman(-1, 0); break;
+				case 'a': movePacman(0, -1); break;
+				case 's': movePacman(1, 0); break;
+				case 'd': movePacman(0, 1);
+			}
+			repaint();
+		}
+		
+		public void keyReleased(KeyEvent e) {}
+		public void keyTyped(KeyEvent e) {}
+	}
+
+	class MonsterLoop implements ActionListener {
+		// Move monster in the cardinal directions
+		public void actionPerformed(ActionEvent e) {
+			for (byte row = 0; row < 10; row++) {
+				for (byte col = 0; col < 10; col++) {
+					byte[] status = board[row][col];
+					// if there are monsters
+					if (status[1] > 0)
+						for (byte mon = 0; mon < status[1]; mon++) {
+							Random randGenerator = new Random();
+							byte newrow = row, newcol = col;
+							// Move monster
+							do {
+								byte randNum = (byte) (randGenerator.nextInt(3)-1); // random number in set {-1,0,1}
+								if (Math.random() >= .5)
+									newrow += randNum;
+								else
+									newcol += randNum;
+							} while(newrow > 9 || newrow < 0 || newcol > 9 || newcol < 0); // ensure new position is on board
+							// remove monster from board
+							board[row][col][1]--;
+							// add new monster to board
+							board[newrow][newcol][1]++;
+						}
+				}
+			}
+			panel.repaint();
+		}
+	}
+	
+	class PacmanLoop implements ActionListener {
+		// Animate pacman's mouth
+		public void actionPerformed(ActionEvent e) {
+			if (isOpeningMouth) {
+				pacman[2]++;
+				// Start CHOMP
+				if (pacman[2] >= 40) isOpeningMouth = false;
+			} else {
+				pacman[2]--;
+				// Breathe for air
+				if (pacman[2] <= 0) isOpeningMouth = true;
+			}
+			panel.repaint();
+		}
+	}
+}
